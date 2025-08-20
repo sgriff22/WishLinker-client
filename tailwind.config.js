4c64fd51e4c57634ce800f9a19da761627032bfc
Stacy Griffy <sgriffy2@gmail.com>
2025-08-19 21:16:29 -0500

    Adjusted css to be responsive to various screen sizes of the welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: "#c15b75",
+        pink: "#e2a5b2",
+        lightRose: "#e19fae",
+      },
+    },
   },
   plugins: [],
-}
+};
