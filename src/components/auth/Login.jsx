4a62a4224e857f1e7e361422967ab99a484fe3df
Stacy Griffy <sgriffy2@gmail.com>
2025-08-20 21:56:17 -0500

    Fixed CSS to be responive for login and register pages

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -37,15 +37,10 @@ export const Login = () => {
   };
 
   return (
-    <main className="container--login mt-20">
-      <dialog className="dialog dialog--auth" ref={existDialog}>
-        <div>User does not exist</div>
-        <button
-          className="button--close"
-          onClick={() => existDialog.current.close()}
-        >
-          Close
-        </button>
+    <main>
+      <dialog className="w-52 rounded-xl py-5 px-2" ref={existDialog}>
+        <div className="text-lg">User does not exist</div>
+        <button onClick={() => existDialog.current.close()}>Close</button>
       </dialog>
 
       <section>
@@ -53,39 +48,45 @@ export const Login = () => {
           className="form--login border shadow-md bg-white rounded-lg"
           onSubmit={handleLogin}
         >
-          <h1 className="text-center mt-7">WishLinker</h1>
-          <h2 className="text-xl mb-2 text-center">Please sign in</h2>
-          <fieldset className="mb-4">
-            <label htmlFor="inputUsername"> Username </label>
-            <input
-              type="username"
-              id="inputUsername"
-              value={username}
-              onChange={(evt) => setUsername(evt.target.value)}
-              className="form-control"
-              placeholder="Username"
-              required
-              autoFocus
-            />
-          </fieldset>
-          <fieldset className="mb-4">
-            <label htmlFor="inputPassword"> Password </label>
-            <input
-              type="password"
-              id="inputPassword"
-              value={password}
-              onChange={(evt) => setPassword(evt.target.value)}
-              className="form-control"
-              placeholder="Password"
-            />
-          </fieldset>
-          <fieldset className="text-right">
-            <button>Sign in</button>
-          </fieldset>
+          <h1 className="text-center mt-7 text-2xl sm:text-4xl md:text-5xl">
+            WishLinker
+          </h1>
+          <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl mb-2 text-center">
+            Please sign in
+          </h2>
+          <div className="w-3/4 mx-auto">
+            <fieldset className="mb-4 text-lg sm:text-2xl">
+              <label htmlFor="inputUsername"> Username </label>
+              <input
+                type="username"
+                id="inputUsername"
+                value={username}
+                onChange={(evt) => setUsername(evt.target.value)}
+                className="form-control"
+                placeholder="Username"
+                required
+                autoFocus
+              />
+            </fieldset>
+            <fieldset className="mb-4 text-lg sm:text-2xl">
+              <label htmlFor="inputPassword"> Password </label>
+              <input
+                type="password"
+                id="inputPassword"
+                value={password}
+                onChange={(evt) => setPassword(evt.target.value)}
+                className="form-control"
+                placeholder="Password"
+              />
+            </fieldset>
+            <fieldset className="text-right">
+              <button>Sign in</button>
+            </fieldset>
+          </div>
           <div className="loginLinks">
             <section className="link--register">
               <Link
-                className="text-lg underline over:text-red-800"
+                className="text-sm sm:text-lg underline over:text-red-800"
                 to="/register"
               >
                 Not a member yet?
